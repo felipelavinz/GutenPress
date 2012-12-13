3d6e9edfaa1ba757fde21d90994720b10844aaf9
Felipe Lavín <felipe@yukei.net>
2012-12-13 18:25:44 -0300

    moved some config definitions to local-config-sample (language, debug, content directory)

--- local-config-sample.php
+++ local-config-sample.php
@@ -10,3 +10,23 @@ define( 'DB_NAME', 'local-db-name' );
 define( 'DB_USER', 'local-db-user' );
 define( 'DB_PASSWORD', 'local-db-password' );
 define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
+
+// ==================================================
+// Log Errors
+// Should be turned on for development
+// Also, super-useful when using the Debug Bar plugin
+// ==================================================
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_LOG', true );
+
+// ================================
+// Language
+// Leave blank for American English
+// ================================
+// define( 'WPLANG', '' );
+
+// ======================================================
+// Custom Content Directory
+// Define accordingly if your site it's on a subdirectory
+// ======================================================
+// define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/{path-to}/content' );
--- wp-config.php
+++ wp-config.php
@@ -13,11 +13,13 @@ if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
 	define( 'DB_HOST', '%%DB_HOST%%' ); // Probably 'localhost'
 }
 
-// ========================
+// =====================================================================
 // Custom Content Directory
-// ========================
+// Define WP_CONTENT_URL on local-config.php if site's on a subdirectory
+// =====================================================================
 define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
-define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
+if ( ! defined('WP_CONTENT_URL') )
+	define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
 
 // ================================================
 // You almost certainly do not want to change these
@@ -29,26 +31,19 @@ define( 'DB_COLLATE', '' );
 // Salts, for security
 // Grab these from: https://api.wordpress.org/secret-key/1.1/salt
 // ==============================================================
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
-
+define('AUTH_KEY',         '0XJ?IG.)3!q3M(wt__Eg9k03G[T&Y^=7qH~7yLN}W2#OkdD(h9(+(FdlN{qGi#Bo');
+define('SECURE_AUTH_KEY',  ' E4.=#zXFFLmQ.{kA.(YRLyJ;QA`9N.u,ugR}0=cp5:`ZRm-IHDOF#6~*+#80bG;');
+define('LOGGED_IN_KEY',    'y+|p5E:7p(+((wpn/|UmyDi1Z[]wUVW~!L!E<KMA-N*[.HA 9Ftj)^F#h~zc*FVZ');
+define('NONCE_KEY',        '4(!}2FwX4ub,oMmN+9#QvA~1MlX-oQ2aT^DWY<tG9]$ SaUQ)%HH=QCBmm?J>+ai');
+define('AUTH_SALT',        'D3S,;/Jd?TLkqW6ce/h+>ho7@olew`hXn9^R(VPfh}tJ|KEw<E4#$&gn1=k|Cs5-');
+define('SECURE_AUTH_SALT', '[1*KTpr/|pky-|2ak@gV(eu<Bg9;/rV-`(suTx:TKPR{~+B`&y6L~}!)V||~NKx]');
+define('LOGGED_IN_SALT',   'b9Z#0]upYTCIG&+KXhieaQhmnl4boVxM1dR@;,J=/{JX^Ec(e=OHEw4t@g^uyt-)');
+define('NONCE_SALT',       '{|p4i~!|h;uL;ini3>b7L /I_U-{Q.uGJ)/W|?`Hx!w!ws}rp%qma>A6W4SU<p]t');
 // ==============================================================
 // Table prefix
 // Change this if you have multiple installs in the same database
 // ==============================================================
 $table_prefix  = 'wp_';
-
-// ================================
-// Language
-// Leave blank for American English
-// ================================
-define( 'WPLANG', '' );
 
 // ===========
 // Hide errors
