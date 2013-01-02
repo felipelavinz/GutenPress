9a3787cb9f9c025c6ce6f6d7d6232a9803b73f80
Felipe Lavín <felipe@yukei.net>
2013-01-02 10:09:23 -0300

    some fixes for attachment helper

--- content/mu-plugins/GutenPress/Helpers/Attachment.php
+++ content/mu-plugins/GutenPress/Helpers/Attachment.php
@@ -10,31 +10,40 @@ class Attachment{
 	protected $pathinfo;
 	protected $filesize;
 
+	private $attachment;
+
 	/**
 	 * Construct an attachment object with common info as properties
 	 * @param int $id File object ID on $wpdb->posts
 	 * */
 	public function __construct( $id ){
+		$this->attachment = get_post( (int)$id );
+		if ( ! $this->attachment )
+			throw new \Exception( sprintf( __('File ID %d not found', 'gutenpress'), $id ), 404 );
 		$this->url = wp_get_attachment_url( $id );
 		$this->title = get_the_title( $id );
 		$this->urlinfo = (object)parse_url( $this->url );
 		$this->pathinfo = (object)pathinfo( $this->urlinfo->path );
 		$this->filesize = static::getSize( $id );
-		// $this->size = $this->weight;
+		// @todo : get a friendy document type (e.g. "presentation", "text", "spreadsheet")
 		// if ( $mime ) $this->type = the_doc_type($mime, false);
 	}
+	/**
+	 * Magic __get so attributes can be protected and read-only
+	 */
 	public function __get( $key ){
-		return $key;
+		return $this->{ $key };
 	}
 	/**
 	 * Get the file size of a given attachment
 	 * @param int|object $id A WordPress attachment ID
 	 * @param string $unit Measure unit. Will default to a sane human-readable unit
 	 * @return string Human-readable file size
+	 * @todo manually specify the unit
 	 */
 	public static function getSize( $attachment, $unit = '' ){
 		$filepath = get_post_meta( $attachment, '_wp_attached_file', true );
-		$fullpath = WP_CONTENT_DIR .'/'. $filepath;
+		$fullpath = WP_CONTENT_DIR .'/uploads/'. $filepath;
 		if( ! is_readable($fullpath) )
 			return '';
 		$size = filesize($fullpath);
