3c4f04397ec4c58ebf46ef9d512bfd5877d5689e
Felipe Lavín <felipe@yukei.net>
2013-01-07 17:44:57 -0300

    inline documentation for PostQuery.php

--- content/mu-plugins/GutenPress/Model/PostQuery.php
+++ content/mu-plugins/GutenPress/Model/PostQuery.php
@@ -3,14 +3,58 @@
 namespace GutenPress\Model;
 
 abstract class PostQuery implements \Iterator, \Countable{
+
+	/**
+	 * Holds the instance of WP_Query which we'll iterate over
+	 * @access private
+	 */
 	private $query;
+
+	/**
+	 * Holds an instance of the current post decorator
+	 * @access private
+	 */
 	private $the_post;
+
+	/**
+	 * Keep a copy of the global post, for those times when wp_reset_postdata doesn't work
+	 * @access private
+	 */
 	private $_wp_post;
+
+	/**
+	 * The post type slug for this type of query
+	 * @access private
+	 */
 	private $post_type;
-	private $decorator;
+
+	/**
+	 * An array of arguments passed to WP_Query
+	 * @access private
+	 */
 	private $query_args;
+
+	/**
+	 * An array of metadata definitions
+	 * @access protected
+	 */
 	protected $metadata;
+
+	/**
+	 * The FQN of a decorator for WP_Post
+	 * @access protected
+	 */
+	protected $decorator;
+
+	/**
+	 * An array of meta fields that should be interpreted as multiple
+	 * @access private
+	 */
 	private $are_multiple;
+
+	/**
+	 * @param array $query_args An array of arguments passed on to WP_Query
+	 */
 	final public function __construct( array $query_args = array() ){
 		$this->query_args = $query_args;
 		$this->post_type = $this->setPostType();
@@ -23,29 +67,55 @@ abstract class PostQuery implements \Iterator, \Countable{
 		}
 	}
 
+	/**
+	 * Set the post type for this type of query
+	 * @return string A post type slug
+	 */
 	abstract protected function setPostType();
 
+	/**
+	 * Set the name for the WP_Post decorator
+	 * @return string The FQN of a WP_Post decorator. It could be empty
+	 */
 	abstract protected function setDecorator();
 
+	/**
+	 * Parse query params and execute WP_Query
+	 * @return void
+	 */
 	private function getObjects(){
 		$query_args = wp_parse_args( $this->query_args, array(
 			'post_type' => $this->post_type
 		) );
 		$this->query = new \WP_Query( $query_args );
 	}
-	// so we can use this on alredy-made queries
+
+	/**
+	 * Re-use an existing WP_Query (for instance, on archive templates where it doesn't make sense to repeat the query)
+	 * @param object WP_Query object
+	 * @throws \Exception Fails if $this->query was already set
+	 */
 	final public function setQuery( \WP_Query $query ){
+		if ( $this->query instanceof \WP_Query ) {
+			throw new \Exception( sprintf( __('The query for this object was already set. You might want to create a new %s', 'gutenpress'), get_called_class() ) );
+		}
 		$this->query = $query;
 		$this->post_type = $this->query->get('post_type');
 		$this->preLoop();
 	}
+
+	/**
+	 * Return the current WP_Query
+	 * @return object The current WP_Query
+	 */
 	public function getQuery(){
 		return $this->query;
 	}
 
 
 	/**
-	 * Countable
+	 * Implement countable interface, so we can do count($object)
+	 * @return int The number of posts on the current query
 	 */
 	public function count(){
 		if ( !isset( $this->query ) ) {
