392cbfc2c7af219a603c9e92d42d70f6984d8ecb
Felipe Lavín <felipe@yukei.net>
2013-01-15 10:42:10 -0300

    added __isset to PostObject and some comments

--- content/mu-plugins/GutenPress/Model/PostObject.php
+++ content/mu-plugins/GutenPress/Model/PostObject.php
@@ -3,26 +3,51 @@
 namespace GutenPress\Model;
 
 class PostObject{
+	/**
+	 * @var object WP_Post
+	 */
 	protected $post;
+
+	/**
+	 * Holds information about multiple meta data
+	 * @var array
+	 */
 	protected $multiple;
+
+	/**
+	 * @var array
+	 */
 	protected $postmeta;
+
 	public function __construct( \WP_Post $post, array $metadata = array(), array $multiple = array() ){
 		$this->post = $post;
 		$this->multiple = $multiple;
 		$this->postmeta = $metadata;
 	}
+
 	public function __get( $key ){
 		if ( $key === 'thumbnail' ) {
 			return $this->getThumbnail();
 		}
 		if ( $key === 'permalink' ) {
-			return get_permalink( $this->post->ID );
+			$this->permalink = get_permalink( $this->post->ID );
+			return $this->permalink;
 		}
 		if ( !empty($this->multiple[ $key ]) ) {
 			return get_post_meta( $this->post->ID, $key, false );
 		}
 		return $this->post->{$key};
 	}
+
+	/**
+	 * Check if some property exists
+	 * @param string The name of the checked property
+	 * @return bool
+	 */
+	public function __isset( $key ){
+		return metadata_exists( 'post', $this->post->ID, $key );
+	}
+
 	public function getThumbnail( $size = 'post-thumbnail', $attr = array() ){
 		return get_the_post_thumbnail( $this->post->ID, $size, $attr );
 	}
