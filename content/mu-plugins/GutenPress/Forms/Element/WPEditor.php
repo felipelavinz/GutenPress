a5389ac7d97e6393aff3c95958bd84324dac7c53
Felipe Lavín <felipe@yukei.net>
2013-01-15 18:42:26 -0300

    wordpress editor form element

---
+++ content/mu-plugins/GutenPress/Forms/Element/WPEditor.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace GutenPress\Forms\Element;
+
+class WPEditor extends \GutenPress\Forms\FormElement{
+	// wordpress defaults
+	private $editor_settings = array(
+		'wpautop'       => true,
+		'media_buttons' => true,
+		'textarea_name' => '',
+		'textarea_rows' => '',
+		'tabindex'      => '',
+		'editor_css'    => '',
+		'editor_class'  => '',
+		'teeny'         => false,
+		'dfw'           => false,
+		'tinymce'       => true,
+		'quicktags'     => true
+	);
+	public function __construct( $label, $name, array $properties = array() ){
+		parent::__construct( $label, $name, $properties );
+		if ( ! isset($properties['textarea_name']) )
+			$properties['textarea_name'] = $name;
+		$this->editor_settings = wp_parse_args( $properties, $this->editor_settings );
+	}
+	private function getSanitizedId(){
+		$id = $this->getAttribute( 'id' ) . $this->name;
+		$id = preg_replace( '/[^a-z]/', '', $id );
+		return strtolower( $id );
+	}
+	public function __toString(){
+		ob_start();
+		wp_editor( $this->getValue(), $this->getSanitizedId(), $this->editor_settings );
+		return ob_get_clean();
+	}
+}
