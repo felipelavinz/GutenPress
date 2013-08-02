d882cb68582ac20a7b7d446c546137d075ac8069
Felipe Lavín Z <felipe@yukei.net>
2013-08-02 17:38:54 -0400

    some improvements on Gmaps element

--- GutenPress/Assets/Javascript/Forms-Element-GMapSearch.js
+++ GutenPress/Assets/Javascript/Forms-Element-GMapSearch.js
@@ -4,21 +4,21 @@
 		var defaults = {
 			'zoom' : 16,
 			'bounds' : false
-		}
-
+		};
 		this.each( function(){
 			var obj;
 			obj = this;
-
+			$obj = $(obj);
 			obj.init = function(){
-				var mapID = $(obj).find('.mapCanvas').attr('id');
-				obj.mapCanvas = $(obj).find('#'+mapID);
-				obj.mapAddress = $(obj).find('.mapAddress');
-				obj.mapSearch = $(obj).find('.mapSearch');
-				obj.mapLat = $(obj).find('.mapLat');
-				obj.mapLng = $(obj).find('.mapLng');
-				obj.mapError = $(obj).find('.mapError');
-			}
+				var mapID = $obj.find('.mapCanvas').attr('id');
+				obj.mapCanvas = $obj.find('#'+mapID);
+				obj.mapAddress = $obj.find('.mapAddress');
+				obj.mapSearch = $obj.find('.mapSearch');
+				obj.mapLat = $obj.find('.mapLat');
+				obj.mapLng = $obj.find('.mapLng');
+				obj.mapError = $obj.find('.mapError');
+				obj.mapReset = $obj.find('.mapReset');
+			};
 			obj.initMap = function(){
 				if( obj.getLat() && obj.getLng() ){
 					// Set Options to center
@@ -28,22 +28,22 @@
 				} else {
 					obj.mapCanvas.gmap({ 'zoom': defaults.zoom });
 				}
-			}
+			};
 			obj.setOptions = function(){
 				var inputPosition = new google.maps.LatLng( obj.getLat(), obj.getLng() );
 				obj.mapCanvas.gmap('get','map').setOptions({ 'zoom': defaults.zoom, 'center': inputPosition });
-			}
+			};
 			obj.getLat = function(){
 				return ( obj.mapLat.val().length ? obj.mapLat.val() : false );
-			}
+			};
 			obj.getLng = function(){
 				return ( obj.mapLng.val().length ? obj.mapLng.val() : false );
-			}
+			};
 			obj.triggerSearch = function(){
-				obj.mapSearch.on("click", function(){
+				obj.mapSearch.on('click', function(){
 					obj.searchAddress();
 				});
-			}
+			};
 			obj.searchAddress = function(){
 				var geocoder = new google.maps.Geocoder();
 				var address = obj.mapAddress.attr('value');
@@ -66,7 +66,17 @@
 						}
 					});
 				}
-			}
+			};
+			obj.resetMap = function(){
+				obj.mapReset.on('click', function(){
+					obj.resetAddress();
+				});
+			};
+			obj.resetAddress = function(){
+				obj.mapAddress.val('');
+				obj.mapLat.val('');
+				obj.mapLng.val('');
+			};
 			obj.setMarker = function(){
 				var inputPosition = new google.maps.LatLng( obj.getLat(), obj.getLng() );
 				obj.mapCanvas
@@ -77,17 +87,18 @@
 						// Set Options to center
 						obj.setOptions();
 					});
-			}
+			};
 			obj.setHiddenLatLng = function( location ){
 				obj.mapLat.val( location.jb );
 				obj.mapLng.val( location.kb );
-			}
+			};
 			// Init Load
 			obj.init();
 			obj.initMap();
 			obj.triggerSearch();
-		})
-	}
+			obj.resetMap();
+		});
+	};
 	$(document).ready( function($){
 		$('.gp-gmapsearch').gpGMapSearch();
 	});
