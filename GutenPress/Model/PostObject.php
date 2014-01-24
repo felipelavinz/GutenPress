4c4dcb8d1c8dca2bc5c979ac6806d22dcf306a9c
Felipe Lavín Z <felipe@yukei.net>
2014-01-24 20:58:00 -0300

    small improvements and fixes for the PostObject model

--- GutenPress/Model/PostObject.php
+++ GutenPress/Model/PostObject.php
@@ -1,43 +1,43 @@
 <?php
-
 namespace GutenPress\Model;
 
 class PostObject{
 	/**
-	 * @var object WP_Post
+	 * Hold a copy of the original WP_Post object
+	 * @var WP_Post
 	 */
 	protected $post;
 
 	/**
-	 * Holds information about multiple meta data
+	 * Hold a copy of the object properties accessed with the magic getter
 	 * @var array
 	 */
-	protected $multiple;
-
-	private $know_multiples = false;
-
 	private $known_properties = array();
 
-	/**
-	 * @var array
-	 */
-	protected $postmeta;
-
-	public function __construct( \WP_Post $post, array $metadata = array(), array $multiple = array() ){
+	public function __construct( \WP_Post $post ){
 		$this->post = $post;
-		if ( ! empty($metadata) ) {
-			$this->postmeta = $metadata;
-		}
-		if ( ! empty($multiple) ) {
-			$this->multiple = $multiple;
-			$this->know_multiples = true;
-		}
 	}
 
+	/**
+	 * Magically get a property from the post object
+	 *
+	 * When a property it's accessed through the magic getter, we'll
+	 * try to get it using get_post_meta() but on that point we don't
+	 * know if it's a single or multiple property.
+	 * This method will get the data and return as a string if it has
+	 * a single value or as an array if it has more than one value
+	 *
+	 * @param  string $key Name of the property, ie a "meta_key"
+	 * @return string|arr Value for the meta property
+	 */
 	public function __get( $key ){
 		// if we already got that property, inmediately return it
 		if ( isset($this->known_properties[ $key ]) ) {
 			return $this->known_properties[ $key ];
+		}
+		// if it's a post field, return that
+		if ( isset($this->post->{$key}) ) {
+			return $this->post->{$key};
 		}
 		if ( $key === 'thumbnail' ) {
 			$this->known_properties['thumbnail'] = $this->getThumbnail();
@@ -47,26 +47,20 @@ class PostObject{
 			$this->known_properties['permalink'] = get_permalink( $this->post->ID );
 			return $this->known_properties['permalink'];
 		}
-		if ( $this->know_multiples === true ) {
-			// we already know what fields can have multiple values
-			if ( $this->multiple[ $key ] ) {
-				$this->known_properties[ $key ] = get_post_meta( $this->post->ID, $key, false );
-				return $this->known_properties[ $key ];
-			}
+		// we don't know which values could have multiple values
+		// so... let's check that
+		$value = get_post_meta( $this->post->ID, $key, false );
+		if ( empty($value) ) {
+			// probably not a postmeta, let the magic in WP_Post handle it
 			return $this->post->{$key};
+		} elseif ( count($value) === 1 ) {
+			// return as a single field
+			// (could be an array itself if data is serialized)
+			$this->known_properties[ $key ] = $value[0];
 		} else {
-			// we don't know, so it _might_ have multiple values
-			$value = get_post_meta( $this->post->ID, $key, false );
-			if ( empty($value) ) {
-				// probably not a postmeta, let WP_Post handle it
-				return $this->post->{$key};
-			} elseif ( count($value) === 1 ) {
-				$this->known_properties[ $key ] = $value[0];
-			} else {
-				$this->known_properties[ $key ] = $value;
-			}
-			return $this->known_properties[ $key ];
+			$this->known_properties[ $key ] = $value;
 		}
+		return $this->known_properties[ $key ];
 	}
 
 	/**
@@ -79,6 +73,14 @@ class PostObject{
 	}
 
 	/**
+	 * Return the instance of the WP_Post
+	 * @return WP_Post The post object that we're working with
+	 */
+	public function getPost(){
+		return $this->post;
+	}
+
+	/**
 	 * Get a given property as an array
 	 * @param string $key The meta_key to get
 	 * @return array The meta value
@@ -87,6 +89,21 @@ class PostObject{
 		return get_post_meta( $this->post->ID, $key, false );
 	}
 
+	/**
+	 * Get a given property as a string
+	 * @param  string $key The meta_key value we're trying to get
+	 * @return string The meta value
+	 */
+	public function getSingle( $key ){
+		return get_post_meta( $this->post->ID, $key, true );
+	}
+
+	/**
+	 * Get the post thumbnail for the current entry
+	 * @param  string $size The image size name
+	 * @param  array  $attr Attributes used on the HTML image element
+	 * @return string Post thumbnail HTML
+	 */
 	public function getThumbnail( $size = 'post-thumbnail', $attr = array() ){
 		return get_the_post_thumbnail( $this->post->ID, $size, $attr );
 	}
